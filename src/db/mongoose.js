a42c0c1088a2610c92f5839935602b3225b67efe
Rizal <rizalbario3@gmail.com>
2023-06-04 13:08:18 +0700

    first commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const CONFIG = require('../config/config');
 
+mongoose.set('strictQuery', false);
+
 mongoose.connect(CONFIG.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
